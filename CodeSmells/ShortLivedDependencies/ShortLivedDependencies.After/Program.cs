2167347cc38c0619744e8a90526e2a640d125fab
andriyan-krastev <andriyan.krastev@fourth.com>
2020-01-22 11:52:43 +0200

    added Short live dependencies examples

---
+++ CodeSmells/ShortLivedDependencies/ShortLivedDependencies.After/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ShortLivedDependencies.After
+{
+    class Program
+    {
+        public static void Main()
+        {
+            IConnectionFactory facotry = new ConnectionFactory();
+
+            using (IConnectionService connection = facotry.GetConnectionService())
+            {
+                connection.GetConnection();
+            }
+        }
+    }
+
+    public class ConnectionService : IConnectionService
+    {
+        public void GetConnection() => Console.WriteLine("Get connection");
+        public void Dispose() => Console.WriteLine("Dispose");
+    }
+    public interface IConnectionService : IDisposable
+    {
+        void GetConnection();
+    }
+
+    public class ConnectionFactory : IConnectionFactory
+    {
+        public IConnectionService GetConnectionService() => new ConnectionService();
+    }
+    public interface IConnectionFactory
+    {
+        IConnectionService GetConnectionService();
+    }
+
+}
---
+++ CodeSmells/ShortLivedDependencies/ShortLivedDependencies.Before/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ShortLivedDependencies.Before
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (IConnectionService connection = new ConnectionService())
+            {
+                connection.GetConnection();
+            }
+        }
+    }
+
+    public class ConnectionService : IConnectionService
+    {
+        public void GetConnection() => Console.WriteLine("Get connection");
+        public void Dispose() => Console.WriteLine("Dispose");
+    }
+    public interface IConnectionService : IDisposable
+    {
+        void GetConnection();
+    }
+}
