3c7452a7ed72f7251968f5cf3fa6fdc2189b5c68
andriyan-krastev <andriyan.krastev@fourth.com>
2020-03-02 16:09:25 +0200

    added comment

--- AOP/AOP.Domain/4.FixLSPintroducedInStep3WithGenericAbstraction/ICommandService.cs
+++ AOP/AOP.Domain/4.FixLSPintroducedInStep3WithGenericAbstraction/ICommandService.cs
@@ -8,6 +8,8 @@ namespace AOP.Domain._4.FixLSPintroducedInStep3WithGenericAbstraction
     // Main reason for the generic ICommandService<TCommand> is to prevent violating the LSP in its clients.
 
     // Changing the non-generic ICommandService into the generic ICommandService<TCommand> fixes our last SOLID violation.
+
+    // This refactoring may be replaced with MediatR library 
     public interface ICommandService<TCommand>
     {
         void Execute(TCommand command);
